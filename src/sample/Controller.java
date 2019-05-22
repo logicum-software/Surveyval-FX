39161c2cd36966933fab717e7ef3606a336e3bac
m.gaertner <m.gaertner@CVB0090>
2019-05-22 13:00:31 +0200

    20190522 13:00

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+    public Label labelOutput;
+
+    public void handleButtonAction(ActionEvent actionEvent) {
+
+        labelOutput.setText("Hello World");
+
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Manager.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(root, 800, 690);
+
+        Stage stage = new Stage();
+        stage.setTitle("Fragebogen-Manager");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
