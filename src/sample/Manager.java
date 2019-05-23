64f9898aa10b52b178c38e50a1ddade06bd22a4c
m.gaertner <m.gaertner@CVB0090>
2019-05-23 10:06:00 +0200

    20190523 10:05

--- src/sample/Manager.java
+++ src/sample/Manager.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.embed.swing.JFXPanel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Manager {
+
+    @FXML
+    public Button buttonClose;
+
+    public void handleButtonCloseAction(ActionEvent actionEvent) {
+
+        // get a handle to the stage
+        Stage stage = (Stage) buttonClose.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
 }
